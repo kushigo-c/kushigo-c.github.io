388a3feac081d1340cd9b9bec307157983ca9f9b
kushigo <kushigo.c@gmail.com>
2023-10-15 16:13:19 +0900

    フェードアップを実装

---
+++ js/fadeup.js
@@ -0,0 +1,13 @@
+$(window).scroll(function () {
+    $(".fadeUpTrigger").each(function () {
+        var elemPos = $(this).offset().top - 50;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll >= elemPos - windowHeight) {
+            $(this).addClass("fadeUp");
+        }
+        else {
+            $(this).removeClass("fadeUp");
+        }
+    });
+});
